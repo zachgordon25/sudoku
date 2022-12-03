cc0ef87e819d2fde628c641cb7860b3d65ce802f
Zach Gordon <zachgordon25@gmail.com>
2022-12-03 13:10:17 -0500

    finished LocalStorageImpl

--- src/main/java/sudoku/buildlogic/SudokuBuildLogic.java
+++ src/main/java/sudoku/buildlogic/SudokuBuildLogic.java
@@ -1,6 +1,7 @@
 package sudoku.buildlogic;
 
 import sudoku.computationlogic.GameLogic;
+import sudoku.persistence.LocalStorageImpl;
 import sudoku.problemdomain.IStorage;
 import sudoku.problemdomain.SudokuGame;
 import sudoku.userinterface.IUserInterfaceContract;
