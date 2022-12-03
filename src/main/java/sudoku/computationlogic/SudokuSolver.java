b1ce570deb4b43a1bb4cfedcca5d0f4cb6afe07d
Zach Gordon <zachgordon25@gmail.com>
2022-12-03 11:52:43 -0500

    wrote typeWriterEnumerate() to check for empty cells

--- src/main/java/sudoku/computationlogic/SudokuSolver.java
+++ src/main/java/sudoku/computationlogic/SudokuSolver.java
@@ -1,4 +1,29 @@
 package sudoku.computationlogic;
 
+import sudoku.problemdomain.Coordinates;
+
+import static sudoku.problemdomain.SudokuGame.GRID_BOUNDARY;
+
 public class SudokuSolver {
+
+    public static boolean puzzleIsSolvable(int[][] puzzle) {
+        Coordinates[] emptyCells = typeWriteEnumerate(puzzle);
+    }
+
+    private static Coordinates[] typeWriteEnumerate(int[][] puzzle) {
+        Coordinates[] emptyCells = new Coordinates[40];
+        int iterator = 0;
+        for (int y = 0; y < GRID_BOUNDARY; y++) {
+            for (int x = 0; x < GRID_BOUNDARY; x++) {
+                if (puzzle[x][y] == 0) {
+                    emptyCells[iterator] = new Coordinates(x, y);
+                    if (iterator == 39) {
+                        return emptyCells;
+                    }
+                    iterator++;
+                }
+            }
+        }
+        return emptyCells;
+    }
 }
