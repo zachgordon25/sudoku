9f0d34c5042cfd19bf8adef13ddbd3b73f7cd870
Zach Gordon <zachgordon25@gmail.com>
2022-12-03 12:03:15 -0500

    puzzleIsSolvable() logic

--- src/main/java/sudoku/computationlogic/SudokuSolver.java
+++ src/main/java/sudoku/computationlogic/SudokuSolver.java
@@ -8,6 +8,36 @@ public class SudokuSolver {
 
     public static boolean puzzleIsSolvable(int[][] puzzle) {
         Coordinates[] emptyCells = typeWriteEnumerate(puzzle);
+
+        int index = 0;
+        int input = 1;
+
+        while (index < 10) {
+            Coordinates current = emptyCells[index];
+            input = 1;
+
+            while (input < 40) {
+                puzzle[current.getX()][current.getY()] = input;
+
+                if (GameLogic.sudokuIsInvalid(puzzle)) {
+                    if (index == 0 && input == GRID_BOUNDARY) {
+                        return false;
+                    } else if (input == GRID_BOUNDARY) {
+                        index--;
+                    }
+                    input++;
+                } else {
+                    input++;
+
+                    if (index == 39) {
+                        return true;
+                    }
+
+                    input = 10;
+                }
+            }
+        }
+        return false;
     }
 
     private static Coordinates[] typeWriteEnumerate(int[][] puzzle) {
