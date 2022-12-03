eff301aaada42244e879683a6c492d3de7262b61
Zach Gordon <zachgordon25@gmail.com>
2022-12-03 12:53:10 -0500

    wrote the build logic

--- src/main/java/sudoku/SudokuApplication.java
+++ src/main/java/sudoku/SudokuApplication.java
@@ -1,9 +1,8 @@
 package sudoku;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sudoku.buildlogic.SudokuBuildLogic;
 import sudoku.userinterface.IUserInterfaceContract;
 import sudoku.userinterface.UserInterfaceImpl;
 
